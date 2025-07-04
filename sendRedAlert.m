36c2ea8d6d8aeb2397369cbe9933b9cee985740a
Hirdhay20 <hirdhay.ramnarain@optinum.co.za>
2025-07-04 12:19:22 +0200

    Initial Commit: Add all project files

---
+++ sendRedAlert.m
@@ -0,0 +1,16 @@
+
+function sendRedAlert(imgName)
+
+
+setpref('Internet','E_mail','Sender')
+setpref('Internet', 'SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username','Sender');
+setpref('Internet','SMTP_Password','Password');
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class','javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+props.setProperty('mail.smtp.starttls.enable','true');
+props.setProperty('mail.smtp.starttls.enable','true');
+sendmail('Recipient','detected red','Red object has been detected', imgName);
+end

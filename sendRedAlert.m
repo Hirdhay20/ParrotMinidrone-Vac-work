aa65f8a383862516e94915ac5dc3f5b3ecfd466c
Hirdhay20 <hirdhay.ramnarain@optinum.co.za>
2025-07-13 14:59:09 +0200

    Update sendRedAlert.m

--- sendRedAlert.m
+++ sendRedAlert.m
@@ -2,10 +2,10 @@
 function sendRedAlert(imgName)
 
 % Setup email preferences for Gmail
-setpref('Internet','E_mail','monkeydavids105@gmail.com')
+setpref('Internet','E_mail','Sender')
 setpref('Internet', 'SMTP_Server','smtp.gmail.com');
-setpref('Internet','SMTP_Username','monkeydavids105@gmail.com');
-setpref('Internet','SMTP_Password','egoj ybyo mgmb wjaq');
+setpref('Internet','SMTP_Username','Sender');
+setpref('Internet','SMTP_Password','App password');
 props = java.lang.System.getProperties;
 props.setProperty('mail.smtp.auth','true');
 props.setProperty('mail.smtp.socketFactory.class','javax.net.ssl.SSLSocketFactory');
@@ -13,5 +13,5 @@ props.setProperty('mail.smtp.socketFactory.port','465');
 props.setProperty('mail.smtp.starttls.enable','true');
 props.setProperty('mail.smtp.starttls.enable','true');
 % Send the email with subject and image attachment 
-sendmail('monkeydavids105@gmail.com','detected red','Red object has been detected', imgName);
+sendmail('Recipient','detected red','Red object has been detected', imgName);
 end
